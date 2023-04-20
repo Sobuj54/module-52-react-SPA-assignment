61d12981fca58cb1d23fd680b6b13c5eab7669ce
soubj <sobujahmed124@gmail.com>
2023-04-20 22:35:31 +0600

    unnecessary error

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>this is header component.</h2>
+        </div>
+    );
+};
+
+export default Header;
