9db694cf331e2093df83fd4f1bb65766264a1934
soubj <sobujahmed124@gmail.com>
2023-04-21 12:27:08 +0600

    fixed first user image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,8 @@ const Header = () => {
     return (
         <div className='header'>
             <h2>Knowledge Cafe</h2>
-            <img src="newcomer-get-know-teammates-pleased-good-looking-businesswoman-transparent-glasses-blue-collar-shirt-smiling-broadly-assuring-good-quality-product-gray-wall" alt="" />
+            {/* <img src="newcomer-get-know-teammates-pleased-good-looking-businesswoman-transparent-glasses-blue-collar-shirt-smiling-broadly-assuring-good-quality-product-gray-wall" alt="" /> */}
+            <p style={{backgroundImage:`url('https://www.freepik.com/free-photo/bohemian-man-with-his-arms-crossed_1157693.htm#query=people&position=1&from_view=search&track=sph')`}}></p>
         </div>
     );
 };
